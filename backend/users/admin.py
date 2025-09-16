66a5200810f9691562f1306f7acdf171f7ddf4e7
Michael Goke <mikhzobby@gmail.com>
2025-09-16 20:05:26 +0100

    prep for guest seeding

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,56 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
 from .models import Profile, User
 
-# Register your models here.
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "groups")
+    search_fields = ("email")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),  # Replace these with your fields
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                ),  # Add custom fields
+            },
+        ),
+    )
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    filter_horizontal = ("groups", "user_permissions")
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Profile)
